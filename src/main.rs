59d61cc0a723f24cd72402a8c2b12402d817f391
c10t <c10t@fa.ke>
2020-01-23 04:11:33 +0900

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+  // let endpoint = "https://hacker-news.firebaseio.com/v0";
+  // let topstories = endpoint.clone().to_owned() + "/topstories.json";
+  let res = reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json").await?;
+  println!("status = {:?}", res.status());
+  let body = res.text().await?;
+  println!("body:\n\n{}", body);
+
+  Ok(())
 }
