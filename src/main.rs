f7846c4754616f58080014b22d0ab1f23bc242a2
c10t <c10t@fa.ke>
2020-01-26 23:55:12 +0900

    use iter

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ extern crate reqwest;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-  // let endpoint = "https://hacker-news.firebaseio.com/v0";
-  // let topstories = endpoint.clone().to_owned() + "/topstories.json";
-  let res = reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json").await?;
+  let endpoint = "https://hacker-news.firebaseio.com/v0";
+  let topstories = endpoint.clone().to_owned() + "/topstories.json";
+  let res = reqwest::get(&topstories).await?; // NG: get(topstories).await?
   println!("status = {:?}", res.status());
   let stories: Vec<u32> = res.json().await?;
-  println!("body:\n\n{:?}", stories);
 
+  let stories = stories.into_iter();
+  stories.take(3).for_each(|id| println!("{:?}", id));
   Ok(())
 }
