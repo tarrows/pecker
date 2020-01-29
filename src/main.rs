019312988a230c1cc9d6b2f38e1998d8301001fe
c10t <c10t@fa.ke>
2020-01-30 04:20:54 +0900

    multiple request version

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 extern crate reqwest;
+// if no use StreamExt, said "note: the method `map` exists but the following trait bounds were not satisfied"
+use futures::{stream, StreamExt};
+use serde_json::json;
+const PARALLEL_REQUESTS: usize = 2;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
   let endpoint = "https://hacker-news.firebaseio.com/v0";
   let topstories = endpoint.clone().to_owned() + "/topstories.json";
   let res = reqwest::get(&topstories).await?; // NG: get(topstories).await?
-  println!("status = {:?}", res.status());
+
   let stories: Vec<u32> = res.json().await?;
 
-  let mut stories = stories.into_iter();
-  let id = stories.next().unwrap();
-  println!("id = {:?}", id);
-  let item = format!("{}/item/{:?}.json", endpoint, id);
-  let res = reqwest::get(&item).await?;
-  println!("status = {:?}", res.status());
-  let story: serde_json::Value = res.json().await?; // NG: let story = ...
+  let bodies = stream::iter(stories)
+    .take(3)
+    .map(|id| {
+      let item = format!("{}/item/{:?}.json", endpoint, id);
+      async move {
+        let res = reqwest::get(&item).await?;
+        let json = res.json::<serde_json::Value>().await; // cannot infer type for T
+        json
+      }
+    })
+    .buffer_unordered(PARALLEL_REQUESTS);
 
-  println!("{:#?}", story);
+  bodies
+    .for_each(|b| {
+      async {
+        match b {
+          Ok(b) => println!(
+            "{:#?}",
+            b.get("title").unwrap_or(&json!({ "title": "<NONE>" }))
+          ),
+          Err(e) => println!("Error: {}", e),
+        }
+      }
+    })
+    .await;
+
   Ok(())
 }
