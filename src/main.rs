de5bab571481bceae7e7e6d04db4fa62d48c1650
c10t <c10t@fa.ke>
2020-01-30 23:55:39 +0900

    split out json value to String

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate reqwest;
-// if no use StreamExt, said "note: the method `map` exists but the following trait bounds were not satisfied"
+
 use futures::{stream, StreamExt};
 const PARALLEL_REQUESTS: usize = 2;
 
@@ -7,7 +7,7 @@ const PARALLEL_REQUESTS: usize = 2;
 async fn main() -> Result<(), reqwest::Error> {
   let endpoint = "https://hacker-news.firebaseio.com/v0";
   let topstories = endpoint.clone().to_owned() + "/topstories.json";
-  let res = reqwest::get(&topstories).await?; // NG: get(topstories).await?
+  let res = reqwest::get(&topstories).await?;
 
   let stories: Vec<u32> = res.json().await?;
 
@@ -17,7 +17,7 @@ async fn main() -> Result<(), reqwest::Error> {
       let item = format!("{}/item/{:?}.json", endpoint, id);
       async move {
         let res = reqwest::get(&item).await?;
-        let json = res.json::<serde_json::Value>().await; // cannot infer type for T
+        let json = res.json::<serde_json::Value>().await;
         json
       }
     })
@@ -27,11 +27,7 @@ async fn main() -> Result<(), reqwest::Error> {
     .for_each(|b| {
       async {
         match b {
-          Ok(b) => {
-            let id = b.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
-            let title = b.get("title").and_then(|x| x.as_str()).unwrap_or("");
-            println!("{}: {}", id, title);
-          }
+          Ok(b) => println!("{}", form(b)),
           Err(e) => println!("Error: {}", e),
         }
       }
@@ -39,4 +35,10 @@ async fn main() -> Result<(), reqwest::Error> {
     .await;
 
   Ok(())
+}
+
+fn form(j: serde_json::Value) -> String {
+  let id = j.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
+  let title = j.get("title").and_then(|x| x.as_str()).unwrap_or("");
+  format!("{}: {}", id, title)
 }
