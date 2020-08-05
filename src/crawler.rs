ff22c11c3104d86d85be7c4fe85c3f9f71a56d5c
c10t <c10t@fa.ke>
2020-08-05 23:46:42 +0900

    WIP creating module

---
+++ src/crawler.rs
@@ -0,0 +1,49 @@
+extern crate reqwest;
+
+use futures::{stream, StreamExt};
+const PARALLEL_REQUESTS: usize = 2;
+
+#[derive(Deserialize, Debug)]
+pub struct Story {
+  id: i64,
+  by: String,
+}
+
+pub fn crawl(n: i32) -> Result<(), reqwest::Error> {
+  let endpoint = "https://hacker-news.firebaseio.com/v0";
+  let topstories = endpoint.clone().to_owned() + "/topstories.json";
+  let res = reqwest::get(&topstories).await?;
+
+  let stories: Vec<u32> = res.json().await?;
+
+  let bodies = stream::iter(stories)
+    .take(n)
+    .map(|id| {
+      async move {
+        let item = format!("{}/item/{:?}.json", endpoint, id);
+        let res = reqwest::get(&item).await?;
+        let json = res.json::<serde_json::Value>().await;
+        json
+      }
+    })
+    .buffer_unordered(PARALLEL_REQUESTS);
+
+  bodies
+    .for_each(|b| {
+      async {
+        match b {
+          Ok(b) => println!("{}", form(b)),
+          Err(e) => println!("Error: {}", e),
+        }
+      }
+    })
+    .await;
+
+  Ok(())
+}
+
+fn form(j: serde_json::Value) -> String {
+  let id = j.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
+  let title = j.get("title").and_then(|x| x.as_str()).unwrap_or("");
+  format!("{}: {}", id, title)
+}
