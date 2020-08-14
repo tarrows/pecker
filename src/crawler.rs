fee15521b18fcaf1f2dfeb940abf6e7187b769ea
c10t <c10t@fa.ke>
2020-08-14 23:16:32 +0900

    move fetch process to lib

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,12 +1,12 @@
 extern crate reqwest;
-
 use futures::{stream, StreamExt};
+use serde::Deserialize;
 const PARALLEL_REQUESTS: usize = 2;
 
 #[derive(Deserialize, Debug)]
 pub struct Story {
   id: i64,
-  type: String,
+  r#type: String,
   by: String,
   descendants: i64,
   kids: Option<Vec<i64>>,
@@ -16,7 +16,7 @@ pub struct Story {
   url: Option<String>,
 }
 
-pub fn crawl(n: i32) -> Result<(), reqwest::Error> {
+pub async fn crawl(n: usize) -> Result<(), reqwest::Error> {
   let endpoint = "https://hacker-news.firebaseio.com/v0";
   let topstories = endpoint.clone().to_owned() + "/topstories.json";
   let res = reqwest::get(&topstories).await?;
@@ -29,7 +29,7 @@ pub fn crawl(n: i32) -> Result<(), reqwest::Error> {
       async move {
         let item = format!("{}/item/{:?}.json", endpoint, id);
         let res = reqwest::get(&item).await?;
-        let json = res.json::<serde_json::Value>().await;
+        let json = res.json::<Story>().await;
         json
       }
     })
@@ -39,7 +39,7 @@ pub fn crawl(n: i32) -> Result<(), reqwest::Error> {
     .for_each(|b| {
       async {
         match b {
-          Ok(b) => println!("{}", form(b)),
+          Ok(b) => println!("{} {}", b.id, b.title),
           Err(e) => println!("Error: {}", e),
         }
       }
@@ -49,8 +49,8 @@ pub fn crawl(n: i32) -> Result<(), reqwest::Error> {
   Ok(())
 }
 
-fn form(j: serde_json::Value) -> String {
-  let id = j.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
-  let title = j.get("title").and_then(|x| x.as_str()).unwrap_or("");
-  format!("{}: {}", id, title)
-}
+// fn form(j: serde_json::Value) -> String {
+//   let id = j.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
+//   let title = j.get("title").and_then(|x| x.as_str()).unwrap_or("");
+//   format!("{}: {}", id, title)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
 extern crate reqwest;
-
-use futures::{stream, StreamExt};
-const PARALLEL_REQUESTS: usize = 2;
+use pecker::crawler::crawl;
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-  let endpoint = "https://hacker-news.firebaseio.com/v0";
-  let topstories = endpoint.clone().to_owned() + "/topstories.json";
-  let res = reqwest::get(&topstories).await?;
-
-  let stories: Vec<u32> = res.json().await?;
-
-  let bodies = stream::iter(stories)
-    .take(3)
-    .map(|id| {
-      async move {
-        let item = format!("{}/item/{:?}.json", endpoint, id);
-        let res = reqwest::get(&item).await?;
-        let json = res.json::<serde_json::Value>().await;
-        json
-      }
-    })
-    .buffer_unordered(PARALLEL_REQUESTS);
-
-  bodies
-    .for_each(|b| {
-      async {
-        match b {
-          Ok(b) => println!("{}", form(b)),
-          Err(e) => println!("Error: {}", e),
-        }
-      }
-    })
-    .await;
-
-  Ok(())
-}
-
-fn form(j: serde_json::Value) -> String {
-  let id = j.get("id").and_then(|x| x.as_i64()).unwrap_or(-1);
-  let title = j.get("title").and_then(|x| x.as_str()).unwrap_or("");
-  format!("{}: {}", id, title)
+async fn main() {
+  let res = crawl(3).await;
+  println!("{:?}", res);
 }
